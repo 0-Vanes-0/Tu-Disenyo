63915754d12e4f9b37ff90981067a41210d085cd
Ivan <malyshev.ivan@gmail.com>
2026-01-21 13:35:52 +0100

    Profile

--- app/src/main/java/com/example/tudisenyo/MainActivity.java
+++ app/src/main/java/com/example/tudisenyo/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.tudisenyo;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bottomNavView = findViewById(R.id.bottomNavView);
+        bottomNavView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment = null;
+                if (menuItem.getItemId() == R.id.menu_profile) {
+                    selectedFragment = new ProfileFragment();
+                } else if (menuItem.getItemId() == R.id.menu_editor) {
+
+                } else if (menuItem.getItemId() == R.id.menu_library) {
+
+                } else if (menuItem.getItemId() == R.id.menu_purchase) {
+
+                }
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                }
+                return true;
+            }
+        });
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+        }
     }
 }
